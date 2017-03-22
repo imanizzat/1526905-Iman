ce1738265d8c1515cda95b62c48354b6594dd63f
imanizzat <1526905@chester.ac.uk>
2017-03-22 16:11:22 +0800

    Updated Contact page

---
+++ WebApplication4/Contact.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="Contact.aspx.cs" Inherits="WebApplication4.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <form action="/action_page.php">
+  First name: 
+        <br />
+        <input type="text" name="fname" id="txtFirstname"><br>
+  Last name:<br />
+        <input type="text" name="lname1" id="txtLastname" size="20"><br>
+  Email Address:&nbsp;
+        <br />
+        <input type="text" name="lname" id="txtEmail" itemid="txtEmail"><br>
+        <br />
+  Message: <br />
+        <input type="text" name="lname" id="txtMessage" style="height: 101px; width: 390px"><br>
+  &nbsp;<asp:Button ID="txtSubmit" runat="server" OnClick="Button1_Click" Text="Submit" />
+</form>
+  
+</asp:Content>
---
+++ WebApplication4/Contact.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication4
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        private object txtName;
+        private object txtMessage;
+        private object txtEmail;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.EnableSsl = true;
+            smtpClient.UseDefaultCredentials = false;
+            System.Net.NetworkCredential credentials =
+                new System.Net.NetworkCredential("imanizzatfarhan@gmail.com", "password");
+            smtpClient.Host = "smtp.gmail.com";
+            smtpClient.Port = 587;
+
+            MailMessage msg = new MailMessage("imanizzatfarhan@gmail.com", txtEmail.Text);
+            msg.Subject = "Name: " + txtFirstname.Text + "Subject: " + txtMessage.Text;
+            smtpClient.Send(msg);
+        }
+    }
+}
